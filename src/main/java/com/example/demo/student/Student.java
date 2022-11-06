979662789375ed9394dae2bc0faf0e8f0e3a3d09
Łukasz Kozioł <adikaktus@gmail.com>
2022-11-06 17:33:58 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,82 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    //region Variables
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+//endregion
+    //region Construktors
+    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+    //endregion
+
+//region Setter
+    public Student() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    //endregion
+
+//region getter
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
+
